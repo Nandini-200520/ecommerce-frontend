83a9b09a5eca825035c12f1a2f3300715727ea6b
Nandini Tanneeru <nandinitanneeru70@gmail.com>
2026-06-02 16:33:24 +0530

    first commit

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,22 @@
-console.log("E-Commerce Website Loaded");
+document.addEventListener("DOMContentLoaded", () => {
+
+    // HAMBURGER MENU 
+    const hamburger = document.querySelector(".hamburger");
+    const navLinks = document.querySelector(".nav-links");
+
+    hamburger.addEventListener("click", () => {
+        navLinks.classList.toggle("active");
+    });
+
+    // CART COUNTER
+    let count = 0;
+    const cartCount = document.querySelector(".cart-count");
+
+    document.querySelectorAll(".add").forEach(btn => {
+        btn.addEventListener("click", () => {
+            count++;
+            cartCount.textContent = count;
+        });
+    });
+
+});
